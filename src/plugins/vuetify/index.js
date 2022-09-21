81fa3a4aac1764fc6bc88f323495a1eb56d9b0a3
XiaoRIGE <1554019387@QQ.COM>
2022-09-21 23:02:53 +0800

    feat: 移除preset style

--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
-import preset from './default-preset/preset'
+// import preset from './default-preset/preset'
 
 Vuetify.config.silent = false // 此选项将停止 所有的 Vuetify 日志和警告
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  preset,
+  // preset,
   icons: {
     iconfont: 'mdiSvg',
   },
